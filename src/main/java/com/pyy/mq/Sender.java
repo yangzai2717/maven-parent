44016e2306581375dc25a817d5d2eb811d43ab99
pyy <869849673@qq.com>
2018-05-02 17:36:31 +0800

    session.createSession参数说明

--- src/main/java/com/pyy/mq/Sender.java
+++ src/main/java/com/pyy/mq/Sender.java
@@ -30,6 +30,22 @@ public class Sender {
             //启动
             connection.start();
             // 获取操作连接
+            /**
+             *
+             createSession(paramA,paramB);
+
+             paramA 取值有 : true or false 表示是否支持事务
+             paramB 取值有:Session.AUTO_ACKNOWLEDGE，Session.CLIENT_ACKNOWLEDGE，DUPS_OK_ACKNOWLEDGE，SESSION_TRANSACTED
+
+             createSession(paramA,paramB);
+             paramA是设置事务的，paramB设置acknowledgment mode
+             paramA设置为false时：paramB的值可为Session.AUTO_ACKNOWLEDGE，Session.CLIENT_ACKNOWLEDGE，DUPS_OK_ACKNOWLEDGE其中一个。
+             paramA设置为true时：paramB的值忽略， acknowledgment mode被jms服务器设置为SESSION_TRANSACTED 。
+             Session.AUTO_ACKNOWLEDGE为自动确认，客户端发送和接收消息不需要做额外的工作。
+             Session.CLIENT_ACKNOWLEDGE为客户端确认。客户端接收到消息后，必须调用javax.jms.Message的acknowledge方法。jms服务器才会删除消息。
+             DUPS_OK_ACKNOWLEDGE允许副本的确认模式。一旦接收方应用程序的方法调用从处理消息处返回，会话对象就会确认消息的接收；而且允许重复确认。在需要考虑资源使用时，这种模式非常有效。
+
+             */
             session = connection.createSession(Boolean.FALSE,
                     Session.AUTO_ACKNOWLEDGE);
             destination = session.createQueue("first-queue");
