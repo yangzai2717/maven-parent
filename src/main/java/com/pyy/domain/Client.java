44e100e4fd49ac5f93f3f36544227e652fb29cae
pyy <869849673@qq.com>
2018-06-05 19:03:41 +0800

    反射学习

--- src/main/java/com/pyy/domain/Client.java
+++ src/main/java/com/pyy/domain/Client.java
@@ -13,6 +13,18 @@ public class Client {
 
     @Test
     public void client(){
+        String str = "{\n" +
+                "  \"objects\":[\n" +
+                "    {\n" +
+                "      \"id\":\"id1\",\n" +
+                "      \"class\":\"com.pyy.domain.User\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"id\":\"id2\",\n" +
+                "      \"class\":\"com.pyy.domain.Bean\"\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
         ObjectPool pool = ObjectPool.init("config.json");
         User user = (User)pool.getObject("id1");
         System.out.println(user.toString());
